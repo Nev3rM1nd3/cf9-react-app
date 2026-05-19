f1f2ce11932cbb786d602af6df17f40c5b925675
Nev3rM1nd3 <panorg264@gmail.com>
2026-05-19 19:45:09 +0300

    feat: add AutoDirectAdvanced component

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,8 @@ import Layout from "./components/Layout";
 // import CounterWithCustomHook from "./components/LessonFive/CounterWithCustomHook.tsx";
 // import {useEffect} from "react";
 // import NameChangerWithTitle from "./components/LessonFive/NameChangerWithTitle.tsx";
-import AutoRedirect from "./components/LessonSix/AutoRedirect.tsx";
+// import AutoRedirect from "./components/LessonSix/AutoRedirect.tsx";
+import AutoRedirectAdvanced from "./components/LessonSix/AutoRedirectAdvanced.tsx";
 // import Counter from "./components/LessonFour/Counter.tsx";
 // import ClassCounter from "./components/LessonFour/ClassCounter.tsx";
 
@@ -120,7 +121,8 @@ function App() {
 
     {/*LESSON SIX*/}
     <Layout addClasses="bg-gray-50">
-      <AutoRedirect/>
+      {/*<AutoRedirect/>*/}
+      <AutoRedirectAdvanced/>
     </Layout>
   </>
 
