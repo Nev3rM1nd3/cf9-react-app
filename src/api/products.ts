8db9c1e88ff055a9ed4edaa6d353d787b4177dce
Nev3rM1nd3 <panorg264@gmail.com>
2026-05-28 18:06:55 +0300

    feat(api): add functions to fetch product endpoints

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,18 +1,14 @@
-import {type Product, type ProductFormData} from "../schemas/products"
+import {type Product} from "../schemas/products"
 
 const API_URL = import.meta.env.VITE_API_URL
 const TENANT_ID = import.meta.env.VITE_TENANT_ID
 
 export async function getProducts(): Promise<Product[]> {
-  const res = await fetch(
-    `/${API_URL}/tenants/${TENANT_ID}/products/`)
-    // "tenants/3f357f95-b19a-4f48-86ff-d4224268cd0c/products/")
+  const res = await fetch(`/${API_URL}/tenants/${TENANT_ID}/products/`)
   if (!res.ok) throw new Error("Failed to fetch products")
-
   const data = await res.json()
   console.log("products res: " + data)
-
-  return data
+  return data;
 }
 
 export async function getProduct(id: number): Promise<Product> {
@@ -23,7 +19,17 @@ export async function getProduct(id: number): Promise<Product> {
 
 export async function updateProduct(
   id: number,
-  data: Product) {
+  data: {
+    description?: string | undefined;
+    image?: string | undefined;
+    is_active: boolean;
+    is_favorite: boolean;
+    name: string;
+    price: number;
+    slug: string;
+    sort: number;
+  },
+) {
   const res = await fetch(`/${API_URL}/tenants/${TENANT_ID}/products/${id}`, {
     method: "PUT",
     headers: {"Content-Type": "application/json"},
@@ -33,9 +39,9 @@ export async function updateProduct(
   return await res.json()
 }
 
-export async function createProduct(data: ProductFormData) {
+export async function createProduct(data: Omit<Product, "id">): Promise<Product> {
   const res = await fetch(`/${API_URL}/tenants/${TENANT_ID}/products/`, {
-    method: "PUT",
+    method: "POST",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify(data),
   })
@@ -44,7 +50,7 @@ export async function createProduct(data: ProductFormData) {
 }
 
 export async function deleteProduct(id: number): Promise<void> {
-  const res = await fetch(`/${API_URL}/tenants/${TENANT_ID}/products/${id}`,{
+  const res = await fetch(`/${API_URL}/tenants/${TENANT_ID}/products/${id}`, {
     method: "DELETE",
   })
   if (!res.ok) throw new Error("Failed to delete product")
