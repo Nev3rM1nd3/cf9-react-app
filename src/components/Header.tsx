697c05b39d5a29942f7f515951253192c15be7d5
Nev3rM1nd3 <panorg264@gmail.com>
2026-05-29 20:51:31 +0300

    feat(auth): add AuthButton on Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import {Link} from "react-router";
+import {AuthButton} from "@/components/AuthButton.tsx";
 
 const Header = () => {
   return (
@@ -9,9 +10,10 @@ const Header = () => {
             <img src="https://codingfactory.aueb.gr/sites/all/themes/cf_theme/logo.png"
                  alt="CF Logo" className="my-4 h-16"/>
           </Link>
-          <nav className="flex gap-4 text-white font-medium">
+          <nav className="flex items-center gap-4 text-white font-medium">
             <Link to="/">Home</Link>
-            <Link to="/products">Products</Link>
+            {/*<Link to="/products">Products</Link>*/}
+            <AuthButton/>
           </nav>
         </div>
       </header>
